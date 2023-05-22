895e0bd47da7f0390ad5e01aa889864ae567a37b
Katharine Chui <kwchuiaa@connect.ust.hk>
2023-05-22 20:42:04 +0800

    implement commondt r/w

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
 mod util;
+mod tdudec;
 
 fn main() {
-	test("Katie");
+	test_hash("Katie");
+	test_commondt_read();
+	test_commondt_write();
 }
 
-fn test(in_string: &'static str) {
+fn test_commondt_write(){
+	let commondt = std::fs::read("commondt.sav").unwrap();
+	let mut commondt_decrypted = tdudec::decrypt_save(&commondt);
+	let player_identifier = util::PlayerIdentifier{
+		nickname: String::from("Katie2"),
+		email: String::from("katie@katie.inc"),
+		password: String::from("hahahaha1234"),
+	};
+	util::patch_commondrt(&mut commondt_decrypted, &player_identifier, true).unwrap();
+	let commondt_modified_encrypted = tdudec::encrypt_save(&commondt_decrypted);
+	std::fs::write("commondt.sav.modified", &commondt_modified_encrypted);
+
+	util::patch_commondrt(&mut commondt_decrypted, &player_identifier, false).unwrap();
+	let commondt_modified_encrypted = tdudec::encrypt_save(&commondt_decrypted);
+	std::fs::write("commondt.sav.modified2", &commondt_modified_encrypted);
+}
+
+fn test_commondt_read(){
+	let commondt = std::fs::read("commondt.sav").unwrap();
+	let commondt_decrypted = tdudec::decrypt_save(&commondt);
+	let login = util::read_commondt(&commondt_decrypted).unwrap();
+	print!("nickname: {}\n", login.nickname);
+	print!("email: {}\n", login.email);
+	print!("password: {}\n", login.password);
+}
+
+fn test_hash(in_string: &'static str) {
 	print!("hashing {}\n", in_string);
+	for byte in String::from(in_string).into_bytes(){
+		print!("{:x} ", byte);
+	}
+	print!("\n");
 	let test_string = String::from(in_string).into_bytes();
     let hash = util::hash_byte_string(&test_string);
     for byte in hash{
--- src/util.rs
+++ src/util.rs
@@ -61,3 +61,98 @@ pub fn hash_byte_string(to_be_hashed: &std::vec::Vec<u8>) -> [u8; 4]{
 
 	return (!uVar1).to_be_bytes();
 }
+
+pub struct PlayerIdentifier{
+	pub nickname: String,
+	pub email: String,
+	pub password: String,
+}
+
+pub fn read_commondt(commondt:&std::vec::Vec<u8>) -> Result<PlayerIdentifier, &'static str>{
+	if commondt.len() < 0x10f{
+		return Err("commondt is too small");
+	}
+
+	let nickname = match String::from_utf8(commondt[0x98..0xb8].to_vec()){
+		Ok(s) => s,
+		Err(_) => {return Err("cannot decode nickname from commondt");}
+	};
+	let email = match String::from_utf8(commondt[0xba..0xee].to_vec()){
+		Ok(s) => s,
+		Err(_) => {return Err("cannot decode email from commondt");}
+	};
+	let password = match String::from_utf8(commondt[0xf0..0x10f].to_vec()){
+		Ok(s) => s,
+		Err(_) => {return Err("cannot decode password from commondt");}
+	};
+
+	return Ok(PlayerIdentifier{
+		nickname: nickname,
+		email: email,
+		password: password,
+	});
+}
+
+pub fn patch_commondrt(commondt:&mut std::vec::Vec<u8>, player_identifier: &PlayerIdentifier, online:bool) -> Result<(), &'static str>{
+	if commondt.len() < 0x10f{
+		return Err("commondt is too small");
+	}
+
+	let nickname_bytes = player_identifier.nickname.clone().into_bytes();
+	let email_bytes = player_identifier.email.clone().into_bytes();
+	let password_bytes = player_identifier.password.clone().into_bytes();
+
+	if nickname_bytes.len() > 30{
+		return Err("nickname is longer than 30 bytes");
+	}
+
+	if email_bytes.len() > 50{
+		return Err("email is longer than 50 bytes");
+	}
+
+	if password_bytes.len() > 30{
+		return Err("password is longer than 30 bytes");
+	}
+
+	for i in 0x98..0xb9{
+		commondt[i] = 0;
+	}
+
+	for i in 0xba..0xef{
+		commondt[i] = 0;
+	}
+
+	for i in 0xf0..0x110{
+		commondt[i] = 0;
+	}
+
+	if online{
+		commondt[0x91] = 1;
+	}else{
+		commondt[0x91] = 0;
+	}
+
+	let mut i:usize = 0;
+	while i < nickname_bytes.len() {
+		commondt[i + 0x98] = nickname_bytes[i];
+		i = i + 1;
+	}
+
+	if !online{
+		return Ok(());
+	}
+
+	i = 0;
+	while i < email_bytes.len() {
+		commondt[i + 0xba] = email_bytes[i];
+		i = i + 1;
+	}
+
+	i = 0;
+	while i < password_bytes.len() {
+		commondt[i + 0xf0] = password_bytes[i];
+		i = i + 1;
+	}
+
+	return Ok(());	
+}
